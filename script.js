41f38d2154bfdfb73e38a9cf0b5b241d9bb54e46
Nagy János <nagy@zold.lan>
2025-03-19 20:09:41 +0100

    aa

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Üdv, én vagyok a script!")
-document.getElementsByTagName("div")[0]
-.innerHTML="Helló, helló!"
+// document.getElementsByTagName("div")[0]
+// .innerHTML="Helló, helló!"
 
 let gykosar= document.getElementsByClassName("gykosar")[0]
 
@@ -16,27 +16,39 @@ for (let i = 1; i < 7; i++) {
   let box = document.createElement("div")
   box.className="box"
 
-  let h3= document.createElement("h3")
-  h3.innerHTML="Fruits..."
-  box.appendChild(h3)
-
   let image = document.createElement("img")
   image.src=`./pics/image${i}.jpg`
   image.alt="Gyümölcs"
   box.appendChild(image)
 
+
+  let boxcontent= document.createElement("div")
+  boxcontent.className="boxcontent"
+  box.appendChild(boxcontent)  
+
+  let h3= document.createElement("h3")
+  h3.innerHTML="Fruits..."
+  boxcontent.appendChild(h3)  
+
   let bekezdes = document.createElement("p")
   bekezdes.innerHTML="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, itaque sunt. Incidunt."
-  box.appendChild(bekezdes)
+  boxcontent.appendChild(bekezdes)
 
   let a = document.createElement("a")
   a.href="#"
   a.className="btn"
   a.innerHTML="részletek"
-  box.appendChild(a)
+  boxcontent.appendChild(a)
 
   gykosar.appendChild(box)
 
 }
+
+let boxok = document.getElementsByClassName("box")
+for (let i = 1; i < boxok.length; i=i+2) {
+  boxok[i].classList.add("leftmargin")  
+}
+
+
 // document.getElementsByClassName("gykosar")[0]
 // .innerHTML=s
